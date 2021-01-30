7e79f0875bc0801feca6961efc55b5a0d59be265
Sun Zoo <32688796+gitupwithit@users.noreply.github.com>
2021-01-30 12:42:35 -0500

    barcode now uses font
    barcode now using a font instead of js lib. needs to be tested .. image seems to work now.

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-function submitCardNumber(cardNumber) {
-  var cardNumber = document.getElementById("cardNumber").value;
-  console.log(cardNumber);
-  document.getElementById("bc").src = "https://www.barcodesinc.com/generator/image.php?code=" + cardNumber + "&style=197&type=C128B&width=250&height=60&xres=1&font=3";
+let clickOnBC = false;
+let currentX;
+let currentY;
+var cardNumber
+const bc = document.getElementById("barcode");
+
+bc.addEventListener('mousedown', mouseDown);
+addEventListener('mouseup', mouseUp);
+addEventListener('mousemove', mouseMove);
+
+function mouseDown(e) {
+  clickOnBC = true;
+  e.preventDefault();
+  bc.classList.add('active');
+  clickOnBC = true;
+}
+
+function mouseUp(e) {
+  clickOnBC = false;
+  bc.classList.remove('active');
+}
+
+function mouseMove(e) {
+  if (clickOnBC == false) {
+    return;
+  }
+  currentX = e.pageX;
+  currentY = e.pageY;
+  bc.style.left = currentX - 80 + 'px';
+  bc.style.top = currentY -370 + 'px';
+}
+
+function submitCardNumber(){
+  var newCardNumber = document.getElementById('cardNumber').value;
+  var newCardNumberString = newCardNumber.toString();
+  document.getElementById('bcnum').innerHTML = newCardNumberString.slice(0,5);
+  document.getElementById('mid').innerHTML = ' '  + newCardNumberString.slice(5,10);
+  document.getElementById('bold').innerHTML = ' ' + newCardNumberString.slice(10,14);
+}
+
+function submitUserImage(userImage) {
+  var userImage = document.getElementById("userImage").value;
+  document.getElementById("ui").src = userImage;
+}
+
+function setBroken() {
+  broken = true;
+  window.location = "broken.html"
 }
